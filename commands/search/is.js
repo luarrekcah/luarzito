45e0d00a476951312aa9911598b16c0b66a6df78
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-08 00:38:00 -0500

    [FIXED] Command IS

--- commands/search/is.js
+++ commands/search/is.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require("@discordjs/builders"),
   gis = require("g-i-s"),
   Discord = require("discord.js"),
   config = require("../../config.json"),
-  { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-
+  { EmbedBuilder } = require('discord.js'),
+  { Pagination } = require('pagination.djs');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("is")
@@ -18,77 +18,29 @@ module.exports = {
     ),
   execute(interaction) {
 
-    if (interaction.user.id != config.botConfig.devId)
-      return interaction.reply({
-        content: "Olá! Meu desenvolvedor está trabalhando nesse comando.",
-        ephemeral: true
-      });
-
-    const { client } = interaction;
     const escolha = interaction.options.getString("query");
 
-    const backId = 'back'
-    const forwardId = 'forward'
-    const backButton = new ButtonBuilder({
-      style: ButtonStyle.Secondary,
-      label: 'Voltar',
-      emoji: '⬅️',
-      customId: backId
-    })
-    const forwardButton = new ButtonBuilder({
-      style: ButtonStyle.Secondary,
-      label: 'Avançar',
-      emoji: '➡️',
-      customId: forwardId
-    });
-
-
     gis(escolha, async (error, results) => {
+      const pagination = new Pagination(interaction);
 
-      let currentIndex = 0;
+      const embeds = [];
 
-      const generateEmbed = async start => {
-        return new EmbedBuilder()
+      results.forEach(item => {
+        const newEmbed = new EmbedBuilder()
           .setColor(config.botConfig.themeColor)
           .setAuthor({ name: "i.s - Image Search", iconURL: config.imagesLink.infoEmbed })
-          .setImage(results[start].url)
+          .setImage(item.url)
           .setDescription(
             `Resultados para ${escolha}`
-          )
-          .setFooter({
-            text: `${results.length} resultados / Imagem atual: ${start + 1}`
-          }
           );
-      };
-
-      const embed = await interaction.reply(
-        {
-          embeds: [await generateEmbed(0)],
-          components: currentIndex === 0 ? [new ActionRowBuilder({ components: [forwardButton] })] :
-            [new ActionRowBuilder({ components: [backButton, forwardButton] })]
-        }
-      );
-
-      const collector = interaction.channel.createMessageComponentCollector({
-        filter: i => i.user.id === interaction.user.id
-      })
-
-      collector.on('collect', async interaction => {
-        interaction.customId === backId ? (currentIndex--) : (currentIndex++);
-        await interaction.update({
-          embeds: [await generateEmbed(currentIndex)],
-          components: [
-            new ActionRowBuilder({
-              components: currentIndex === 0 ? [new ActionRowBuilder({ components: [forwardButton] })] :
-                [new ActionRowBuilder({ components: [backButton, forwardButton] })]
-            })
-          ]
-        })
-      })
-
-      collector.on('end', () => {
-        console.log('fim');
+        embeds.push(newEmbed);
       });
+
+      pagination.setEmbeds(embeds);
+      pagination.setEmbeds(embeds, (embed, index, array) => {
+        return embed.setFooter({ text: `Página: ${index + 1}/${array.length}` });
+      });
+      pagination.render();
     });
   }
 };
