490a471438bf62044475436f0cc7af180e24e6d2
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-08 00:17:46 -0500

    [ADDED] Deploy files

--- deploy/tests.js
+++ deploy/tests.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const config = require('./config.json');
+const config = require('../config.json');
 require('dotenv').config();
 
 const commands = [];
@@ -12,15 +12,15 @@ fs.readdirSync('./commands').forEach((dir) => {
 	const commandFiles = fs.readdirSync(`./commands/${dir}`).filter((files) => files.endsWith(".js"));
 	console.log(commandFiles);
 	for (const file of commandFiles) {
-		const command = require(`./commands/${dir}/${file}`);
+		const command = require(`../commands/${dir}/${file}`);
 		commands.push(command.data.toJSON());
-	}	
+	}
 	count++;
 });
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationCommands(config.botConfig.clientId), { body: commands })
-	.then(() => console.log(`[GLOBAL] ${count} Comandos Registrados com Sucesso`))
+rest.put(Routes.applicationGuildCommands(config.botConfig.clientId, config.botConfig.guildId), { body: commands })
+	.then(() => console.log(`[TEST] ${count} Comandos Registrados com Sucesso`))
 	.catch(console.error);
-  
+
