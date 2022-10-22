9f219b7ae12e10155cb59ee3bf03497d12316ab0
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-21 20:14:30 -0500

    [ADDED] ESlint

--- deploy/delete.js
+++ deploy/delete.js
@@ -6,6 +6,6 @@ require('dotenv').config();
 const rest = new REST({ version: '9' }).setToken(config.botConfig.development ? process.env.DEVELOPMENT_TOKEN : process.env.TOKEN);
 
 rest.delete(Routes.applicationCommand(config.botConfig.development ? config.botConfig.devClientId : config.botConfig.clientId, '1033183603275874426'))
-.then(() => console.log('Comando deletado globalmente'))
-.catch(console.error);
+	.then(() => console.log('Comando deletado globalmente'))
+	.catch(console.error);
 
--- deploy/tests.js
+++ deploy/tests.js
@@ -1,5 +1,4 @@
 const fs = require('node:fs');
-const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const config = require('../config.json');
@@ -9,7 +8,7 @@ const commands = [];
 let count = 0;
 console.log(__dirname);
 fs.readdirSync('./commands').forEach((dir) => {
-	const commandFiles = fs.readdirSync(`./commands/${dir}`).filter((files) => files.endsWith(".js"));
+	const commandFiles = fs.readdirSync(`./commands/${dir}`).filter((files) => files.endsWith('.js'));
 	console.log(commandFiles);
 	for (const file of commandFiles) {
 		const command = require(`../commands/${dir}/${file}`);
