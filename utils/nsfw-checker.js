64ffb84ac369002c62550291b0340b37513c6797
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-21 19:24:06 -0500

    [NOT FIXED] Nsfw search filter

--- utils/nsfw-checker.js
+++ utils/nsfw-checker.js
@@ -1,13 +1,12 @@
 const fs = require('node:fs');
 
-const checker = (content) => {
-    fs.readFile('./utils/nsfwlist.txt', 'utf8', function(err, data) {
+function checker(content) {
+   fs.readFile('./utils/nsfwlist.txt', 'utf8', (err, data) => {
         if (err) throw err;
         const wordlist = data.replaceAll("\n", ",").replaceAll("\r", "").split(",");
-        console.log(wordlist);
-        if(wordlist.includes(content)) return true;
-        else return false;
-      });
+        console.log(`includes? ${wordlist.includes(content)}`)
+        return (wordlist.includes(content));
+    });
 }
 
-module.exports = checker;
+module.exports = {checker};
