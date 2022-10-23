8c6cd2b22755c0223b1474c326a28e69161e4d44
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-23 17:04:14 -0500

    [eslint]

--- utils/nsfw-checker.js
+++ utils/nsfw-checker.js
@@ -1,11 +1,11 @@
 const fs = require('node:fs');
 
 module.exports = {
-    checker: function (content) {
-        const wordlist = fs.readFileSync('./utils/nsfwlist.txt', 'utf8')
-            .replaceAll("\n", ",")
-            .replaceAll("\r", "")
-            .split(",");
-        return (wordlist.includes(content.toLowerCase()));
-    }
+	checker: function(content) {
+		const wordlist = fs.readFileSync('./utils/nsfwlist.txt', 'utf8')
+			.replaceAll('\n', ',')
+			.replaceAll('\r', '')
+			.split(',');
+		return (wordlist.includes(content.toLowerCase()));
+	},
 };
