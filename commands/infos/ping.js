d1f2c3829707e78bd03e325eab8a94ea0e5368df
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-24 20:44:23 -0500

    [ADDED] Font Techno

--- commands/infos/ping.js
+++ commands/infos/ping.js
@@ -14,6 +14,8 @@ module.exports = {
 			clientID = client.users.cache.get(config.botConfig.clientId),
 			clientAvatar = `https://cdn.discordapp.com/avatars/${clientID.id}/${clientID.avatar}.png`;
 
+		canvas.registerFont('../../fonts/techno_hideo.ttf', { family: 'techno' });
+
 		let totalSeconds = client.uptime / 1000;
 		const
 			days = Math.floor(totalSeconds / 86400),
@@ -167,7 +169,7 @@ module.exports = {
 		ctx.strokeStyle = '#74037b';
 		ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-		ctx.font = '20px Impact';
+		ctx.font = '20px, techno';
 		ctx.fillStyle = '#ffffff';
 		ctx.fillText(
 			`${uptime}`,
@@ -177,7 +179,7 @@ module.exports = {
 			// 1.5 quanto menos, baixo
 		);
 
-		ctx.font = '20px Impact';
+		ctx.font = '20px, techno';
 		ctx.fillStyle = '#ffffff';
 		ctx.fillText(
 			`${latenciaSV}ms`,
@@ -187,7 +189,7 @@ module.exports = {
 			// 1.5 quanto menos, baixo
 		);
 
-		ctx.font = '20px Impact';
+		ctx.font = '20px, techno';
 		ctx.fillStyle = '#ffffff';
 		ctx.fillText(
 			`${api}ms`,
