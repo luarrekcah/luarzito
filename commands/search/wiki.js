691b75c16ec33f0c751a18d3d5d323392662caba
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-28 20:36:27 -0500

    [FIXES] Some fixes on wiki command

--- commands/search/wiki.js
+++ commands/search/wiki.js
@@ -17,8 +17,7 @@ module.exports = {
 		),
 	async execute(interaction) {
 		const query = interaction.options.getString('query');
-        
-		const { body } = axios.get('https://pt.wikipedia.org/w/api.php', {
+		axios.get('https://pt.wikipedia.org/w/api.php', {
 			params: {
 				action: 'query',
 				prop: 'extracts',
@@ -29,22 +28,21 @@ module.exports = {
 				redirects: '',
 				formatversion: 2,
 			},
+		}).then(async data => {
+			const body = data.data;
+			if (body.query.pages[0].missing) {
+				return await interaction.reply({
+					content: 'Sem resultados.',
+					fetchReply: true,
+					ephemeral: true,
+				});
+			}
+			const embed = new EmbedBuilder()
+				.setTitle('Wiki - ' + body.query.pages[0].title)
+				.setColor(config.botConfig.themeColor)
+				.setDescription(body.query.pages[0].extract.substr(0, 2000).replace(/[\n]/g, '\n\n'));
+
+			return interaction.reply({ embeds: [embed] });
 		});
-
-		if (body.query.pages[0].missing) {
-			return await interaction.reply({
-				content: 'Sem resultados.',
-				fetchReply: true,
-				ephemeral: true,
-			});
-		}
-
-		const embed = new EmbedBuilder()
-			.setAuthor('Wikipedia', 'https://i.imgur.com/a4eeEhh.png')
-			.setTitle('Wiki - ' + body.query.pages[0].title)
-			.setColor(config.botConfig.themeColor)
-			.setDescription(body.query.pages[0].extract.substr(0, 2000).replace(/[\n]/g, '\n\n'));
-
-		return interaction.reply({ embeds: [embed] });
 	},
 };
