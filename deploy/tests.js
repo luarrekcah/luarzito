cc14f0c10c67df2f529eb83e5843c289a6d44a4a
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-18 03:32:48 -0500

    updated deploy global

--- deploy/tests.js
+++ deploy/tests.js
@@ -1,6 +1,5 @@
 const fs = require('node:fs');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { REST, Routes } = require('discord.js');
 const config = require('../config.json');
 require('dotenv').config();
 
