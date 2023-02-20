5ba4d99c9da7c3cec5af113a2fac0b7c3b1c182c
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-20 18:12:29 -0500

    [added] more tokens

--- commands/search/chatgpt.js
+++ commands/search/chatgpt.js
@@ -39,7 +39,7 @@ module.exports = {
 				model: modelName,
 				prompt,
 				temperature: 0.5,
-				max_tokens: 500,
+				max_tokens: 1000,
 				top_p: 1,
 				frequency_penalty: 0,
 				presence_penalty: 0.6,
