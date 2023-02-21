2ed33e26134caa19bc68366292284b6339085d93
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-20 23:27:42 -0500

    [added] about me

---
+++ commands/profile/aboutme.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { updateItem } = require('../../database');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('sobremim')
+		.setDescription(
+			'Conte um pouco sobre você.',
+		)
+		.addStringOption(option =>
+			option
+				.setName('text')
+				.setDescription('Seja breve mas explicativo!')
+				.setRequired(true),
+		),
+	execute(interaction) {
+		const newAbout = interaction.options.getString('text');
+
+		updateItem({
+			path: `users/${interaction.user.id}/profile`,
+			params: {
+				aboutme: newAbout,
+			},
+		});
+
+		return interaction.reply({
+			content: `Seu 'sobre mim' foi atualizado para: ${newAbout}`,
+		});
+	},
+};
