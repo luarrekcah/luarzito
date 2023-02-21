15902c66c78798ee54b6ebaf6049c02bb29da0bc
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-20 22:13:23 -0500

    fixes

--- commands/users/carta.js
+++ commands/users/carta.js
@@ -4,28 +4,27 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('carta')
 		.setDescription('Envie uma mensagem a um usuário através do bot!')
-		.addStringOption(option =>
-			option.setName('id')
-				.setDescription('Id do usuário')
-				.setRequired(true))
-		.addStringOption(option =>
-			option.setName('mensagem')
+		.addStringOption((option) =>
+			option.setName('id').setDescription('Id do usuário').setRequired(true),
+		)
+		.addStringOption((option) =>
+			option
+				.setName('mensagem')
 				.setDescription('Mensagem para o usuário')
-				.setRequired(true)),
+				.setRequired(true),
+		),
 	execute(interaction) {
 		const { client } = interaction,
 			id = interaction.options.getString('id'),
 			message = interaction.options.getString('mensagem');
 		const allMessage = `Carta de <@${interaction.user.id}> - (${interaction.user.id}):\n\n${message}`;
-		try {
-			client.users.send(id, allMessage);
-		}
-		catch (error) {
+
+		client.users.send(id, allMessage).catch(() => {
 			return interaction.reply({
-				content: 'Ocorreu um erro.',
+				content: 'Não foi possível enviar uma carta para esse usuário.',
 				ephemeral: true,
 			});
-		}
+		});
 		return interaction.reply({
 			content: 'Mensagem enviada.',
 			ephemeral: true,
