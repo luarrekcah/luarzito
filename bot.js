774e59f442044c490fb24ed13c2d1e32aeacc288
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-23 11:18:39 -0500

    guild messages

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const config = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent,
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent,
 	GatewayIntentBits.GuildMembers] });
 
 client.commands = new Collection();
