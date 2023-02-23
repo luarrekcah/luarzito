099001d486e13596b1a57da74d37d463b648d779
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-23 01:43:44 -0500

    added command dev

---
+++ commands/dev/setavatar.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require('@discordjs/builders'),
+	config = require('../../config.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('setavatar')
+		.setDescription('Desenvolvedor apenas')
+		.addStringOption(option =>
+			option.setName('avatar')
+				.setDescription('Tipos de avatar')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'normal', value: 'normal' },
+					{ name: 'manutencao', value: 'manutencao' },
+					{ name: 'desenvolvimento', value: 'desenvolvimento' },
+				)),
+	async execute(interaction) {
+		const opt = interaction.options.getString('avatar');
+		const { client } = interaction;
+		if (interaction.user.id != config.botConfig.devId) {
+			return interaction.reply({
+				content: 'Ei, bobinhx! Este comando é apenas para o desenvolvedor.',
+				ephemeral: true,
+			});
+		}
+
+		switch (opt) {
+		case 'normal':
+			await client.user.setAvatar(config.botConfig.avatarsTheme.default);
+			interaction.reply({
+				content: 'Avatar normal setado.',
+				ephemeral: true,
+			});
+			break;
+		case 'manutencao':
+			await client.user.setAvatar(config.botConfig.avatarsTheme.manutencao);
+			interaction.reply({
+				content: 'Avatar manutenção setado.',
+				ephemeral: true,
+			});
+			break;
+		}
+	},
+};
