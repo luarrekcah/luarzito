fe8004dfb1468f83e68fa4dba29e8a7a1400c35c
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-24 09:00:57 -0500

    added afk command

---
+++ commands/users/afk.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { updateItem } = require('../../database');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('afk')
+		.setDescription('Ative ou desative seu afk')
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName('ativar')
+				.setDescription('Ative seu afk')
+				.addStringOption((option) =>
+					option.setName('reason').setDescription('Razão do AFK'),
+				),
+		)
+		.addSubcommand((subcommand) =>
+			subcommand.setName('desativar').setDescription('Desative seu afk'),
+		),
+	async execute(interaction) {
+		const reason = interaction.options.getString('reason');
+		const subcomando = interaction.options.getSubcommand();
+		switch (subcomando) {
+		case 'ativar':
+			updateItem({
+				path: `afk/${interaction.user.id}`,
+				params: {
+					afk: true,
+					reason,
+				},
+			});
+			return interaction.reply({
+				content: `Seu AFK foi ativo com a razão: ${reason}`,
+			});
+		case 'desativar':
+			updateItem({
+				path: `afk/${interaction.user.id}`,
+				params: {
+					afk: false,
+					reason: '',
+				},
+			});
+			return interaction.reply({
+				content: 'Seu AFK foi desativado',
+			});
+		}
+	},
+};
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,9 +1,36 @@
+const { getItems, updateItem } = require('../database');
+
 module.exports = {
 	name: 'messageCreate',
-	async execute(message,
-	) {
+	async execute(message) {
+		if (message.author.bot) return;
 		if (message.content === '<@743841329334845530>') {
-			return message.reply('Olá! Use `/` para usar meus comandos, digite `/ajuda` para mais informações!');
+			return message.reply(
+				'Olá! Use `/` para usar meus comandos, digite `/ajuda` para mais informações!',
+			);
+		}
+
+		const afkData = await getItems({ path: `afk/${message.author.id}` });
+		if (afkData && afkData.afk) {
+			updateItem({
+				path: `afk/${message.author.id}`,
+				params: {
+					afk: false,
+					reason: '',
+				},
+			});
+
+			return message.reply('Seu AFK foi desativado devido sua atividade.');
+		}
+
+		for (let index = 0; index < message.mentions.users.length; index++) {
+			const user = message.mentions.users[index];
+
+			const afkCheck = await getItems({ path: `afk/${user.id}` });
+
+			if (afkCheck && afkCheck.afk) {
+				return message.reply(`O usuários ${user.tag} está AFK. Razão: ${afkCheck.reason}`);
+			}
 		}
 	},
 };
