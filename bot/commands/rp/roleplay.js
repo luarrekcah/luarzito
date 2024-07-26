68535381df619d2bca1a94ddcc5c2a183874a37a
luarrekcah <raulrodriguesdemoraes@gmail.com>
2024-07-26 03:19:36 -0500

    update package and deploy global

--- bot/commands/rp/roleplay.js
+++ bot/commands/rp/roleplay.js
@@ -22,28 +22,16 @@ const roleplays = [
 		title: ':bite: Mordida! :bite:'
 	},
 	{
-		name: 'bleh',
-		description: 'Faça uma careta para alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=bleh',
-		title: ':stuck_out_tongue: Bleh! :stuck_out_tongue:'
-	},
-	{
 		name: 'blush',
 		description: 'Fique envergonhado(a) com alguém!',
 		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=blush',
-		title: ':blush: Envegonhado(a)! :blush:'
+		title: ':blush: Envergonhado(a)! :blush:'
 	},
 	{
 		name: 'brofist',
 		description: 'Dê um soquinho de camaradagem!',
 		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=brofist',
 		title: ':punch: Soquinho! :punch:'
-	},
-	{
-		name: 'celebrate',
-		description: 'Celebre com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=celebrate',
-		title: ':tada: Celebrando! :tada:'
 	},
 	{
 		name: 'cheers',
@@ -56,18 +44,6 @@ const roleplays = [
 		description: 'Aplauda alguém!',
 		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=clap',
 		title: ':clap: Aplausos! :clap:'
-	},
-	{
-		name: 'confused',
-		description: 'Fique confuso(a) com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=confused',
-		title: ':confused: Confuso(a)! :confused:'
-	},
-	{
-		name: 'cool',
-		description: 'Mostre que você é descolado(a)!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=cool',
-		title: ':sunglasses: Descolado(a)! :sunglasses:'
 	},
 	{
 		name: 'cry',
@@ -88,42 +64,6 @@ const roleplays = [
 		title: ':dancer: Dançando! :dancer:'
 	},
 	{
-		name: 'drool',
-		description: 'Babe por alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=drool',
-		title: ':drooling_face: Babando! :drooling_face:'
-	},
-	{
-		name: 'evillaugh',
-		description: 'Dê uma risada maligna!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=evillaugh',
-		title: ':smiling_imp: Risada maligna! :smiling_imp:'
-	},
-	{
-		name: 'facepalm',
-		description: 'Dê um gesto de decepção!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=facepalm',
-		title: ':man_facepalming: Decepcionante... :woman_facepalming:'
-	},
-	{
-		name: 'handhold',
-		description: 'Segure a mão de alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=handhold',
-		title: ':handshake: Mãos dadas! :handshake:'
-	},
-	{
-		name: 'happy',
-		description: 'Fique feliz com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=happy',
-		title: ':smiley: Feliz! :smiley:'
-	},
-	{
-		name: 'headbang',
-		description: 'Batendo a cabeça com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=headbang',
-		title: ':metal: AHHHH! VOCÊ ME DEIXA LOUCO(A) :metal:'
-	},
-	{
 		name: 'hug',
 		description: 'Dê um abraço em alguém!',
 		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=hug',
@@ -142,12 +82,6 @@ const roleplays = [
 		title: ':laughing: Rindo! :laughing:'
 	},
 	{
-		name: 'lick',
-		description: 'Lamba alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=lick',
-		title: ':tongue: Lambida! :tongue:'
-	},
-	{
 		name: 'love',
 		description: 'Mostre amor por alguém!',
 		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=love',
@@ -160,28 +94,10 @@ const roleplays = [
 		title: ':rage: Bravo(a)! :rage:'
 	},
 	{
-		name: 'nervous',
-		description: 'Fique nervoso(a) com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=nervous',
-		title: ':grimacing: Nervoso(a)! :grimacing:'
-	},
-	{
 		name: 'no',
 		description: 'Diga não para alguém!',
 		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=no',
 		title: ':no_entry_sign: Não! :no_entry_sign:'
-	},
-	{
-		name: 'nom',
-		description: 'Coma algo com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=nom',
-		title: ':yum: Nom Nom! :yum:'
-	},
-	{
-		name: 'nosebleed',
-		description: 'Tenha um sangramento nasal!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=nosebleed',
-		title: ':nose: Sangramento nasal! :nose:'
 	},
 	{
 		name: 'nuzzle',
@@ -190,28 +106,10 @@ const roleplays = [
 		title: ':relaxed: Carinho! :relaxed:'
 	},
 	{
-		name: 'nyah',
-		description: 'Faça um som de gatinho para alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=nyah',
-		title: ':cat: Nyah! :cat:'
-	},
-	{
 		name: 'pat',
 		description: 'Faça cafuné em alguém!',
 		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=pat',
 		title: ':sparkling_heart: Owwwn cafunézinho :sparkling_heart:'
-	},
-	{
-		name: 'peek',
-		description: 'Dê uma espiada em alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=peek',
-		title: ':eyes: Espiadinha! :eyes:'
-	},
-	{
-		name: 'pinch',
-		description: 'Belisque alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=pinch',
-		title: ':pinching_hand: Beliscão! :pinching_hand:'
 	},
 	{
 		name: 'poke',
@@ -220,22 +118,10 @@ const roleplays = [
 		title: ':point_right: Cutucada! :point_right:'
 	},
 	{
-		name: 'pout',
-		description: 'Faça beicinho para alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=pout',
-		title: ':pouting_cat: Beicinho! :pouting_cat:'
-	},
-	{
 		name: 'punch',
 		description: 'Dê um soco em alguém!',
 		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=punch',
 		title: ':facepunch: Soco! :facepunch:'
-	},
-	{
-		name: 'run',
-		description: 'Corra com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=run',
-		title: ':running: Correndo! :running:'
 	},
 	{
 		name: 'sad',
@@ -256,118 +142,22 @@ const roleplays = [
 		title: ':shouting: Gritando! :shouting:'
 	},
 	{
-		name: 'shrug',
-		description: 'Dê de ombros para alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=shrug',
-		title: ':shrug: Dando de ombros! :shrug:'
-	},
-	{
-		name: 'shy',
-		description: 'Fique tímido(a) com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=shy',
-		title: ':flushed: Tímido(a)! :flushed:'
-	},
-	{
 		name: 'slap',
 		description: 'Dê um tapa em alguém!',
 		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=slap',
 		title: ':raised_hand: Tapa! :raised_hand:'
 	},
 	{
-		name: 'slowclap',
-		description: 'Dê uma salva de palmas lenta!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=slowclap',
-		title: ':clap: Palmas lentas! :clap:'
-	},
-	{
-		name: 'smack',
-		description: 'Dê uma palmada em alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=smack',
-		title: ':raised_back_of_hand: Palmada! :raised_back_of_hand:'
-	},
-	{
-		name: 'smile',
-		description: 'Sorria para alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=smile',
-		title: ':smile: Sorrindo! :smile:'
-	},
-	{
-		name: 'sorry',
-		description: 'Peça desculpas a alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=sorry',
-		title: ':pleading_face: Desculpa! :pleading_face:'
-	},
-	{
-		name: 'stare',
-		description: 'Encare alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=stare',
-		title: ':eyes: Encarando! :eyes:'
-	},
-	{
-		name: 'stop',
-		description: 'Pare alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=stop',
-		title: ':raised_hand: Parando! :raised_hand:'
-	},
-	{
-		name: 'surprised',
-		description: 'Fique surpreso(a) com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=surprised',
-		title: ':astonished: Surpreso(a)! :astonished:'
-	},
-	{
-		name: 'thumbsup',
-		description: 'Dê um joinha para alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=thumbsup',
-		title: ':+1: Joinha! :+1:'
-	},
-	{
-		name: 'tickle',
-		description: 'Faça cócegas em alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=tickle',
-		title: ':laughing: Cócegas! :laughing:'
-	},
-	{
-		name: 'wave',
-		description: 'Dê um tchauzinho para alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=wave',
-		title: ':wave: Tchauzinho! :wave:'
-	},
-	{
 		name: 'wink',
 		description: 'Pisque para alguém!',
 		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=wink',
 		title: ':wink: Piscadinha! :wink:'
-	},
-	{
-		name: 'woah',
-		description: 'Fique impressionado(a) com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=woah',
-		title: ':open_mouth: Impressionado(a)! :open_mouth:'
-	},
-	{
-		name: 'yawn',
-		description: 'Boceje com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=yawn',
-		title: ':yawning_face: Bocejando! :yawning_face:'
-	},
-	{
-		name: 'yay',
-		description: 'Celebre com alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=yay',
-		title: ':partying_face: Yay! :partying_face:'
-	},
-	{
-		name: 'yes',
-		description: 'Diga sim para alguém!',
-		apiUrl: 'https://api.otakugifs.xyz/gif?reaction=yes',
-		title: ':white_check_mark: Sim! :white_check_mark:'
 	}
 ];
 
 const commandBuilder = new SlashCommandBuilder()
 	.setName('roleplay')
-	.setDescription('Faça um RP super legal! + de 60 RPs');
+	.setDescription('Faça um RP super legal!');
 
 roleplays.forEach(rp => {
 	commandBuilder.addSubcommand((subcommand) =>
--- bot/deploy/global.js
+++ bot/deploy/global.js
@@ -1,22 +1,25 @@
 const fs = require('node:fs');
 const { REST, Routes } = require('discord.js');
-const config = require('../../shared/config.json');
+const path = require('path');
+const config = require('../../website/config');
 require('dotenv').config();
 
 const commands = [];
 let count = 0;
-fs.readdirSync('./commands').forEach((dir) => {
-	const commandFiles = fs.readdirSync(`./commands/${dir}`).filter((files) => files.endsWith('.js') && dir !== 'dev');
+
+const commandsPath = path.join(__dirname, '..', 'commands');
+fs.readdirSync(commandsPath).forEach((dir) => {
+	const commandFiles = fs.readdirSync(path.join(commandsPath, dir)).filter((file) => file.endsWith('.js') && dir !== 'dev');
 	console.log(commandFiles);
 	for (const file of commandFiles) {
-		const command = require(`../commands/${dir}/${file}`);
+		const command = require(path.join(commandsPath, dir, file));
 		commands.push(command.data.toJSON());
 		count++;
 	}
 });
 
-const rest = new REST({ version: '9' }).setToken(config.botConfig.development ? process.env.DEVELOPMENT_TOKEN : process.env.TOKEN);
+const rest = new REST({ version: '9' }).setToken(config.bot.token);
 
-rest.put(Routes.applicationCommands(config.botConfig.development ? config.botConfig.devClientId : config.botConfig.clientId), { body: commands })
+rest.put(Routes.applicationCommands(config.bot.client_id), { body: commands })
 	.then(() => console.log(`[GLOBAL] ${count} Comandos Registrados com Sucesso`))
 	.catch(console.error);
